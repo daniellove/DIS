f0025220ef4ead9c19410f3f7c4845fd3eeeb010
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2023-12-30 20:18:08 +1300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 
 const SHEET_ID = '13cAT4h0YwbZ4s6nQBrU9FUUt-nQjaU9iEAln7GVb5zM';
-const CLIENT_ID = '926782039243-1c74ts1tg8boub4i4k2ql0a35ueste4l.apps.googleusercontent.com';
+// const CLIENT_ID = '926782039243-1c74ts1tg8boub4i4k2ql0a35ueste4l.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyB6A4LdYKP_r0Y7xMnhpAJ1H4aouVb5g5U';
-const SCOPE = 'https://www.googleapis.com/auth/streadsheets';
+// const SCOPE = 'https://www.googleapis.com/auth/streadsheets';
 
 // var params = {
 // 	spreadsheetId: SHEET_ID,
@@ -12,20 +12,24 @@ const SCOPE = 'https://www.googleapis.com/auth/streadsheets';
 // 	insertDataOption: 'INSERT_ROWS',
 // }
 
+var data = {
+
+}
+
 var params = {
-	"dataFilters"; [{
-		"gridRange": {
-			"sheetId": 0,
-			"startColumnIndex": 0,
-			"endColumnIndex": 1
-		}
+	"dataFilters": [{
+		"a1Range": "Characters!A:A"
 	}]
+}
+
+var headers = {
+	Authorization: API_KEY, 'content-type': 'application/json'
 }
 
 $.ajax({
 	type: 'POST',
 	url: `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/developerMetadata:search`,
-	key: API_KEY,
+	headers: headers,
 	data: params,
 	success: function(response) {
 		console.log(response)
