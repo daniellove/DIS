7be97ebebd439b242f780247af2449e7b65a855b
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2024-01-01 23:43:17 +1300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-console.log(9)
+console.log(0)
 
 const API_KEY = 'YD7XPP6efuRAuajXCZMkk3bBtWyqcHNCvvuAlCGGmWYxQI5gFqw-7FtbdPU';
 const SHEET_ID = '13cAT4h0YwbZ4s6nQBrU9FUUt-nQjaU9iEAln7GVb5zM';
@@ -16,52 +16,72 @@ var SHEET_ROWS = [];
 var ROW_SKIPS = 0 
 
 const TEST_DATA = ["333", "test name", "dwarf", "dwarf", "10", "medium", "medium", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
-const TEST_DATA2 = ["444", "test name", "dwarf", "dwarf", "10", "medium", "medium", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"];
 
+$.ajaxSetup({headers: HEADERS})
 function getRows() {
+	$.ajax({
+		type: 'GET',
+		url: HOST_URL,
+		data: {
+			'apiKey': API_KEY,
+			'spreadsheetId': SHEET_ID,
+			'limit': ROW_COUNT.toString(),
+			'skip': ROW_SKIPS.toString(),
+			'no-cache': true
+		},
+		success: (data => processRows(data))
+	});
+}
 
-	var url = new URL(HOST_URL);
-	var params = {
-		'apiKey': API_KEY,
-		'spreadsheetId': SHEET_ID,
-		'limit': ROW_COUNT.toString(),
-		'skip': ROW_SKIPS.toString()
+function processRows(data) {
+	SHEET_ROWS = SHEET_ROWS.concat(data['results']);
+	if (SHEET_HEADERS.length == 0) {
+		for (var column in SHEET_ROWS[0]) {
+			if (column != 'rowIndex') SHEET_HEADERS.push(column);
+		};
 	};
 
-	Object.keys(params).forEach(
-		key => url.searchParams.append(
-			key, encodeURIComponent(params[key])
-		)
-	);
+	console.log(data)
+	console.log(SHEET_HEADERS);
 
-	fetch(url, {'no-cache': true})
-		.then(r => r.json())
-		.then(data => processRows(data));
+	if (data['hasNextPage']) {
+		if (!ROW_SKIPS) ROW_SKIPS = 1
+		ROW_SKIPS = ROW_SKIPS + ROW_COUNT
+		getRows()
+	} else {
+		console.log(SHEET_ROWS);
+	}
 
 	return
-
-	function processRows(data) {
-		SHEET_ROWS = SHEET_ROWS.concat(data['results']);
-		if (SHEET_HEADERS.length == 0) {
-			for (var column in SHEET_ROWS[0]) {
-				if (column != 'rowIndex') SHEET_HEADERS.push(column);
-			};
-		};
-
-		console.log(data)
-		console.log(SHEET_HEADERS);
-
-		if (data['hasNextPage']) {
-			if (!ROW_SKIPS) ROW_SKIPS = 1
-			ROW_SKIPS = ROW_SKIPS + ROW_COUNT
-			getRows()
-		} else {
-			console.log(SHEET_ROWS);
-		}
-
-		return
-	};
 };
+
+
+
+
+// function getRows() {
+
+// 	var url = new URL(HOST_URL);
+// 	var params = {
+// 		'apiKey': API_KEY,
+// 		'spreadsheetId': SHEET_ID,
+// 		'limit': ROW_COUNT.toString(),
+// 		'skip': ROW_SKIPS.toString()
+// 	};
+
+// 	Object.keys(params).forEach(
+// 		key => url.searchParams.append(
+// 			key, encodeURIComponent(params[key])
+// 		)
+// 	);
+
+// 	fetch(url, {'no-cache': true})
+// 		.then(r => r.json())
+// 		.then(data => processRows(data));
+
+// 	return
+
+	
+// };
 
 
 function postRow(data) {
