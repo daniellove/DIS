47c5fdbeda57711bf86547470202f703a3d39261
Daniel Nelmes-Love <dnelmeslove@gmail.com>
2024-02-20 15:46:40 +1300

    updates

--- interactives.js
+++ interactives.js
@@ -35,7 +35,34 @@ $(document).on('click', '.drop_options span', function() {
 });
 
 // Talents
+var shiftdown = false
+$(document).on('keydown', function(e) {
+	if (e['originalEvent']['key'] == 'Shift') shiftdown = true;
+	return;;
+});
+
+$(document).on('keyup', function(e) {
+	if (e['originalEvent']['key'] == 'Shift') shiftdown = false;
+	return;
+});
 
 $(document).on('click', '.talent', function(e) {
-	
+	e.preventDefault();
+
+	var talent = $(this);
+	var tValue = typeof talent.attr('level') != 'undefined' ? +talent.attr('level') : 0;
+	var pValue = typeof talent.attr('point') != 'undefined' ? +talent.attr('point') : 0;
+	var level = +$('#character_level').text();
+
+	if (shiftdown) {
+		if (pValue == 0) return;
+		talent.attr('point', pValue - 1)
+		talent.attr('level', tValue - 1)
+	} else {
+		if (tValue == 3) return;
+		talent.attr('point', pValue + 1)
+		talent.attr('level', tValue + 1)
+	}
+
+	return;	
 });
